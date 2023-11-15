7d4e0460b9e8887ca59e858d4cce316f6d8ad628
StanAubert <41484171+StanAubert@users.noreply.github.com>
2023-11-15 16:08:56 +0100

    init register and login

--- rollhubfront/src/App.js
+++ rollhubfront/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <header>Bienvenu sur Roll Hub !</header>
+        <Home/>
     </div>
   );
 }
--- rollhubfront/src/components/Home.js
+++ rollhubfront/src/components/Home.js
@@ -6,6 +6,14 @@ const Home = () => {
     return (
         <>
             <h1>RollHub</h1>
+            <div>
+                <h3>Login</h3>
+                <form action="">
+                    <input type="email" placeholder={"Email"}/>
+                    <input type="password" placeholder={"Mot de passe"}/>
+                    <button> Connexion </button>
+                </form>
+            </div>
         </>
     )
 }
